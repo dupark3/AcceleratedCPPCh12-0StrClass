5e305690a842849cb1d6b79accd4cfd63828b2bd
Du Park <dupark3@gmail.com>
2018-03-16 06:49:30 -0400

    Added test conditions for default constructor, argument constructor, const char* constructor, and iterator constructor.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@
 
 int main()
 {
+    Str defaultStr;
+    Str argStr(5, 'a');
+    Str nullTermCharArrayStr("hello");
+    Str iteratorStr(argStr.begin(), argStr.end());
 
-    return 0;
+    for (size_t i = 0; i != argStr.size(); ++i)
+        std::cout << argStr[i] << " ";
+    std::cout << std::endl;
+
+    for (size_t i = 0; i != nullTermCharArrayStr.size(); ++i)
+        std::cout << nullTermCharArrayStr[i] << " ";
+    std::cout << std::endl;
+
+    for (size_t i = 0; i != iteratorStr.size(); ++i)
+        std::cout << iteratorStr[i] << " ";
+    std::cout << std::endl;
 }
